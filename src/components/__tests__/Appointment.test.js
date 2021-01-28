931df1b2812b76e84b3345c3f35c624999a549a5
Adam <adam.tranquilla@gmail.com>
2021-01-27 22:35:55 -0700

    code cleaned up 3/3 (weird formatting removed)

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -3,7 +3,6 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import Appointment from "components/Application";
-import Form from "components/Appointment/Form";
 
 describe("Appointment", () => {
   it("renders without crashing", () => {
